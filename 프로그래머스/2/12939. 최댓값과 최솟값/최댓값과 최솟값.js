fc7a5880ec2885869d4a5377b43dcfb31f883194
지원 <giwon.yi339@gmail.com>
2023-12-13 15:19:00 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,7 @@
     function solution(s) {
-   let strArr = s.split(" ").map((num) => Number(num));
-  return Math.min(...strArr) + " " + Math.max(...strArr);
+    let strArr = s
+    .split(" ")
+    .map((num) => Number(num))
+    .sort((a, b) => a - b);
+  return strArr[0] + " " + strArr[strArr.length - 1];
     }
